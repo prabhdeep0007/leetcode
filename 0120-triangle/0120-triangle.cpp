96c2459735d5faac4a5c5a77ec08e3e1eca21cd9
Prabhdeep Singh <89641220+prabhdeep0007@users.noreply.github.com>
2023-06-05 01:40:51 +0530

    Time: 40 ms (8.25%), Space: 9 MB (50.24%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle,int i,int j,int n,vector<vector<int>>&dp)
-    {
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down=triangle[i][j]+solve(triangle,i+1,j,n,dp);
-        int diag=triangle[i][j]+solve(triangle,i+1,j+1,n,dp);
-        return dp[i][j]=min(down,diag);
-    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(triangle,0,0,n,dp);   
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+             if(i==0 && j==0) 
+             {
+                 dp[i][j]=triangle[i][j];
+                 cout<<dp[i][j]<<"  ";
+             }
+             else    
+                {
+                 int up=INT_MAX,rightup=INT_MAX;
+                  if(i>0 && j!=i) up=triangle[i][j]+dp[i-1][j];
+                  if(i>0 &&j>0) rightup=triangle[i][j]+dp[i-1][j-1];  
+                 dp[i][j]=min(up,rightup);
+                 cout<<dp[i][j]<<"  ";
+                }
+                
+            }
+            cout<<endl;
+        }
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            mini=min(dp[n-1][i],mini);
+        }
+        return mini;
     }
 };

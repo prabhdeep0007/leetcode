e0611c451fbec0c291c87463009ee24ff3bd0bd7
Prabhdeep Singh <89641220+prabhdeep0007@users.noreply.github.com>
2023-07-17 20:20:40 +0530

    Time: 28 ms (93.47%), Space: 71.4 MB (35.96%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -20,26 +20,26 @@ public:
         ListNode*temp=dummy;
         while(l1&&l2)
         {
-            sum=(carry+l1->val+l2->val)%10;
-            temp->next=new ListNode(sum);
-            carry=(carry+l1->val+l2->val)/10;
+            sum=carry+l1->val+l2->val;
+            temp->next=new ListNode(sum%10);
+            carry=sum/10;
             l1=l1->next;
             l2=l2->next;
             temp=temp->next;
         }
         while(l1)
         {
-            sum=(carry+l1->val)%10;
-            temp->next=new ListNode(sum);
-            carry=(carry+l1->val)/10;
+            sum=carry+l1->val;
+            temp->next=new ListNode(sum%10);
+            carry=sum/10;
             l1=l1->next;
             temp=temp->next;
         }
         while(l2)
         {
-            sum=(carry+l2->val)%10;
-            temp->next=new ListNode(sum);
-            carry=(carry+l2->val)/10;
+            sum=(carry+l2->val);
+            temp->next=new ListNode(sum%10);
+            carry=sum/10;
             l2=l2->next;
             temp=temp->next;
         }

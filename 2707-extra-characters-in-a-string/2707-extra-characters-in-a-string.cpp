c276e115e4373ffdc9809d23fd269f467fc3c20a
Prabhdeep Singh <89641220+prabhdeep0007@users.noreply.github.com>
2023-09-02 11:13:12 +0530

    Time: 434 ms (18.66%), Space: 207.3 MB (13.17%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -4,14 +4,14 @@ public:
     {
         if(i>=n)return 0;
         if(dp[i]!=-1) return dp[i];
-        int not_Take=solve(s,m,i+1,dp,n),take=0;
+        int not_Take=1+solve(s,m,i+1,dp,n),take=n+1;
         string t="";
         for(int ind=i;ind<n;ind++)
         {
          t+=s[ind];
-            if(m[t]) take=max(take,(int)(t.size()+solve(s,m,ind+1,dp,n)));
+            if(m[t]) take=min(take,solve(s,m,ind+1,dp,n));
         }
-        return dp[i]=max(take,not_Take);
+        return dp[i]=min(take,not_Take);
     }
     int minExtraChar(string s, vector<string>& dictionary) {
      unordered_map<string,int> m;
@@ -22,6 +22,6 @@ public:
     }
         string t="";
         vector<int> dp(n+1,-1);
-     return n-solve(s,m,0,dp,n);
+     return solve(s,m,0,dp,n);
     }
 };

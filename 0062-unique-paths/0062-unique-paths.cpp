dec1b2c9f53fb34786bc5dce2d2e50782e58fdde
Prabhdeep Singh <89641220+prabhdeep0007@users.noreply.github.com>
2023-09-03 14:23:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (24.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,7 +14,27 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> t(m,vector<int>(n,-1));
-        return dp(0,0,m,n,t);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        for(int i=m-1;i>=0;i--){
+            
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1&&j==n-1)
+                    dp[i][j]=1;
+                else if(i==m-1)
+                    dp[i][j]=dp[i][j+1];
+                else if(j==n-1)
+                    dp[i][j]=dp[i+1][j];
+                else dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                
+                
+            }
+            
+            
+        }
+        
+        
+        
+        return dp[0][0];
     }
 };
